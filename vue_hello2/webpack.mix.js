503da6673b033cc8ef3e1aa204b20fed0808c6fb
nousi <nousidayo@gmail.com>
2020-12-26 15:33:07 +0900

    [Update] webpack.mix.js

--- vue_hello2/webpack.mix.js
+++ vue_hello2/webpack.mix.js
@@ -1,3 +1,11 @@
 let mix = require("laravel-mix");
 
-mix.js("resources/js/app.js", "public/js");
+mix
+  .js("resources/js/app.js", "public/js")
+  .sass("resources/sass/app.scss", "public/css", {
+    implementation: require("node-sass"),
+  })
+  .browserSync({
+    files: ["public/**/*.*"],
+    proxy: "http://localhost:8000",
+  });
