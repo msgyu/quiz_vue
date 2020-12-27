019f19ffd2647bcfa69d5a273b52964f55e62d64
nousi <nousidayo@gmail.com>
2020-12-27 20:41:37 +0900

    vueのimport

--- laravel_vue_quiz/resources/js/app.js
+++ laravel_vue_quiz/resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+require("./bootstrap");
+import Vue from "vue";
+import router from "./router.js";
+import SocialSharing from "vue-social-sharing";
+
+Vue.use(SocialSharing);
+
+new Vue({
+    router: router
+}).$mount("#app");
