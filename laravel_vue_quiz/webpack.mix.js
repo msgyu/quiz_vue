897ade8b94b043026a00b98b93d812c5d9bbb3a9
nousi <nousidayo@gmail.com>
2020-12-27 21:00:27 +0900

    axiosのインストール

--- laravel_vue_quiz/webpack.mix.js
+++ laravel_vue_quiz/webpack.mix.js
@@ -1,12 +1,12 @@
 let mix = require("laravel-mix");
 
-mix
-  .js("resources/js/home.js", "public/js")
-  .js("resources/js/quiz.js", "public/js")
-  .sass("resources/sass/app.scss", "public/css", {
-    implementation: require("node-sass"),
-  })
-  .browserSync({
-    files: ["public/**/*.*"],
-    proxy: "http://localhost:8000",
-  });
+mix.js("resources/js/home.js", "public/js")
+    .js("resources/js/quiz.js", "public/js")
+    .js("resources/js/app.js", "public/js")
+    .sass("resources/sass/app.scss", "public/css", {
+        implementation: require("node-sass")
+    })
+    .browserSync({
+        files: ["public/**/*.*"],
+        proxy: "http://localhost:8000"
+    });
