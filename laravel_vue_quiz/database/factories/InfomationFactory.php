4259187e0a310435f411fb31849d1e4d99713fdd
nousi <nousidayo@gmail.com>
2020-12-28 10:10:32 +0900

    RUN "php artisan make:model Models/Infomation -a!

---
+++ laravel_vue_quiz/database/factories/InfomationFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Infomation;
+use Faker\Generator as Faker;
+
+$factory->define(Infomation::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
