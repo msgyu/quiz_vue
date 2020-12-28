f8745182f10b505cb9f3d8d9b8e1b13fb4580ab4
nousi <nousidayo@gmail.com>
2020-12-28 22:58:48 +0900

    Answer.phpに$fillableを追加

--- laravel_vue_quiz/app/Models/Answer.php
+++ laravel_vue_quiz/app/Models/Answer.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    protected $fillable = [
+        'answer_1','answer_2','answer_3', 'answer_4', 'correct_answer_no', 'commentary'
+    ];
+
+    
 }
