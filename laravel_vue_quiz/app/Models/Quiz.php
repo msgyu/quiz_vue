166b56ccb4a7f3d8e8483666fa5a3edba3b6b11d
nousi <nousidayo@gmail.com>
2020-12-28 22:59:04 +0900

    Quiz.phpに$fillableとリレーションを追加

--- laravel_vue_quiz/app/Models/Quiz.php
+++ laravel_vue_quiz/app/Models/Quiz.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quiz extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'image_src', 'answers_id', 'categories_id'
+    ];
+
+    public function answer()
+    {
+        return $this->hasOne('App\Models\Answer');
+    }
+
+    public function category()
+    {
+        return $this->hasOne('App\Models\Category');
+    }
 }
