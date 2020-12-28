e45dfe0c16566de6b9410f7e1e6b6a3506bf616e
nousi <nousidayo@gmail.com>
2020-12-28 10:26:31 +0900

    getCreatedAtAttributeメソッドの追加

--- laravel_vue_quiz/app/Models/Information.php
+++ laravel_vue_quiz/app/Models/Information.php
@@ -3,10 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Information extends Model
 {
     protected $fillable = [
         'information'
     ];
+
+    public function getCreatedAtAttribute($date)
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $date)->format('Y/m/d');
+    }
 }
