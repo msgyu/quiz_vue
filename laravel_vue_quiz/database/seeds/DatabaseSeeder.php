a1fae1a06653c2ad0b358323d3c6164792cefb2d
nousi <nousidayo@gmail.com>
2020-12-29 16:18:03 +0900

    QuizSeederを追加

--- laravel_vue_quiz/database/seeds/DatabaseSeeder.php
+++ laravel_vue_quiz/database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(InformationSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(QuizSeeder::class);
     }
 }
