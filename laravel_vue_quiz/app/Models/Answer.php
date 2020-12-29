65e9d7f93e339a1c664589b9349f235849991b18
nousi <nousidayo@gmail.com>
2020-12-29 16:35:11 +0900

    Update AnswerSeeder

--- laravel_vue_quiz/app/Models/Answer.php
+++ laravel_vue_quiz/app/Models/Answer.php
@@ -7,8 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Answer extends Model
 {
     protected $fillable = [
-        'answer_1','answer_2','answer_3', 'answer_4', 'correct_answer_no', 'commentary'
+        'answer_1', 'answer_2', 'answer_3', 'answer_4', 'correct_answer_no', 'commentary'
     ];
-
-    
 }
