d648ae4979fd397065044fd63f38bd7899b15e04
nousi <nousidayo@gmail.com>
2020-12-29 17:23:42 +0900

    Category.phpにQuizのリレーションを追加

--- laravel_vue_quiz/app/Models/Category.php
+++ laravel_vue_quiz/app/Models/Category.php
@@ -9,4 +9,9 @@ class Category extends Model
     protected $fillable = [
         'name'
     ];
+
+    public function category()
+    {
+        return $this->hasMany('App\Models\Quiz');
+    }
 }
