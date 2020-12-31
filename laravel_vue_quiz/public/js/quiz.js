012cdcda707a93251b8573c41b3df29b46d2a4eb
nousi <nousidayo@gmail.com>
2021-01-01 01:15:08 +0900

    クイズを次の問題に移行できる

--- laravel_vue_quiz/public/js/quiz.js
+++ laravel_vue_quiz/public/js/quiz.js
@@ -230,9 +230,13 @@ __webpack_require__.r(__webpack_exports__);
       commentary: "",
       correctAnswerNo: 0,
       isCorrect: false,
+      //正解かどうか
       isMistake: false,
+      //間違いかどうか
       isAlreadyAnswered: false,
+      //回答済みかどうか
       isQuizFinish: false,
+      //クイズが終了したかどうか
       score: 0,
       quizNumber: 1,
       categoryName: ""
@@ -248,26 +252,57 @@ __webpack_require__.r(__webpack_exports__);
       _this.findNextQuiz(0);
 
       console.log(_this.quizData);
-      console.log("ok");
     });
   },
   methods: {
-    goAnswer: function goAnswer() {
-      console.log("ok");
-    },
-    goNextQuiz: function goNextQuiz() {
-      console.log("ok");
-    },
-    showResult: function showResult() {
-      console.log("ok");
+    goAnswer: function goAnswer(selectAnswerNum) {
+      if (selectAnswerNum === 0) {
+        // selectAnswerNumが0の場合は、click 「正解を表示する」ボタンのクリック alert-info、alert-dangerを非表示
+        this.isCorrect = false;
+        this.isMistake = false;
+      } else if (selectAnswerNum === Number(this.correctAnswerNo)) {
+        // 正解を押した場合 alert-infoを表示し、alert-dangerを非表示にする そしてスコアを加算する
+        this.isCorrect = true;
+        this.isMistake = false;
+        this.score += 1;
+      } else {
+        // 不正解の場合 alert-infoを非表示し、alert-dangerを表示にする
+        this.isMistake = true;
+        this.isCorrect = false;
+      } // 回答済みの設定をONにする 同じ問題に２回以上の回答をさせないため、そして解説を表示するため
+
+
+      this.isAlreadyAnswered = true; // 10問以上回答している場合は、クイズを終了
+
+      if (this.quizNumber >= 10) {
+        this.endQuiz();
+      }
     },
     findNextQuiz: function findNextQuiz(quizNumber) {
       this.title = this.quizData[quizNumber].title;
       this.answers = [this.quizData[quizNumber].answer.answer_1, this.quizData[quizNumber].answer.answer_2, this.quizData[quizNumber].answer.answer_3, this.quizData[quizNumber].answer.answer_4];
-      console.log("ok");
       this.commentary = this.quizData[quizNumber].answer.commentary;
       this.correctAnswerNo = this.quizData[quizNumber].answer.correct_answer_no;
       this.categoryName = this.quizData[quizNumber].category.name;
+    },
+    goNextQuiz: function goNextQuiz() {
+      // 次の問題へをクリック
+      if (this.quizNumber >= 10) {
+        // 10問以上の場合はクイズを終了
+        this.endQuiz();
+      } else {
+        // 次のクイズを表示し、クイズ番号を加算、alert-info、alert-danger、解説を非表示にする
+        this.findNextQuiz(this.quizNumber);
+        this.quizNumber += 1;
+        this.isCorrect = false;
+        this.isMistake = false;
+        this.isAlreadyAnswered = false;
+      }
+    },
+    endQuiz: function endQuiz() {
+      this.isQuizFinish = true;
+      this.answerNo = "-";
+      this.isAlreadyAnswered = true;
     }
   }
 });
--- laravel_vue_quiz/resources/js/components/page/Quiz.vue
+++ laravel_vue_quiz/resources/js/components/page/Quiz.vue
@@ -131,10 +131,10 @@ export default {
             answers: [],
             commentary: "",
             correctAnswerNo: 0,
-            isCorrect: false,
-            isMistake: false,
-            isAlreadyAnswered: false,
-            isQuizFinish: false,
+            isCorrect: false, //正解かどうか
+            isMistake: false, //間違いかどうか
+            isAlreadyAnswered: false, //回答済みかどうか
+            isQuizFinish: false, //クイズが終了したかどうか
             score: 0,
             quizNumber: 1,
             categoryName: ""
@@ -146,18 +146,31 @@ export default {
             this.quizData = response.data;
             this.findNextQuiz(0);
             console.log(this.quizData);
-            console.log("ok");
         });
     },
     methods: {
-        goAnswer() {
-            console.log("ok");
-        },
-        goNextQuiz() {
-            console.log("ok");
-        },
-        showResult() {
-            console.log("ok");
+        goAnswer(selectAnswerNum) {
+            if (selectAnswerNum === 0) {
+                // selectAnswerNumが0の場合は、click 「正解を表示する」ボタンのクリック alert-info、alert-dangerを非表示
+                this.isCorrect = false;
+                this.isMistake = false;
+            } else if (selectAnswerNum === Number(this.correctAnswerNo)) {
+                // 正解を押した場合 alert-infoを表示し、alert-dangerを非表示にする そしてスコアを加算する
+                this.isCorrect = true;
+                this.isMistake = false;
+                this.score += 1;
+            } else {
+                // 不正解の場合 alert-infoを非表示し、alert-dangerを表示にする
+                this.isMistake = true;
+                this.isCorrect = false;
+            }
+            // 回答済みの設定をONにする 同じ問題に２回以上の回答をさせないため、そして解説を表示するため
+            this.isAlreadyAnswered = true;
+
+            // 10問以上回答している場合は、クイズを終了
+            if (this.quizNumber >= 10) {
+                this.endQuiz();
+            }
         },
         findNextQuiz(quizNumber) {
             this.title = this.quizData[quizNumber].title;
@@ -167,12 +180,30 @@ export default {
                 this.quizData[quizNumber].answer.answer_3,
                 this.quizData[quizNumber].answer.answer_4
             ];
-            console.log("ok");
             this.commentary = this.quizData[quizNumber].answer.commentary;
             this.correctAnswerNo = this.quizData[
                 quizNumber
             ].answer.correct_answer_no;
             this.categoryName = this.quizData[quizNumber].category.name;
+        },
+        goNextQuiz() {
+            // 次の問題へをクリック
+            if (this.quizNumber >= 10) {
+                // 10問以上の場合はクイズを終了
+                this.endQuiz();
+            } else {
+                // 次のクイズを表示し、クイズ番号を加算、alert-info、alert-danger、解説を非表示にする
+                this.findNextQuiz(this.quizNumber);
+                this.quizNumber += 1;
+                this.isCorrect = false;
+                this.isMistake = false;
+                this.isAlreadyAnswered = false;
+            }
+        },
+        endQuiz() {
+            this.isQuizFinish = true;
+            this.answerNo = "-";
+            this.isAlreadyAnswered = true;
         }
     }
 };
