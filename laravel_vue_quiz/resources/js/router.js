cf7bc3223dd2c876d3173e08e35afca2107f1363
nousi <nousidayo@gmail.com>
2021-01-01 23:15:33 +0900

    [Update router.js] Add login path

--- laravel_vue_quiz/resources/js/router.js
+++ laravel_vue_quiz/resources/js/router.js
@@ -24,6 +24,11 @@ export default new Router({
             path: "/register",
             name: "register",
             component: Register
+        },
+        {
+            path: "/login",
+            name: "login",
+            component: Login
         }
     ]
 });
