b96da0e936b55b4c582a460870c284679adddd00
nousi <nousidayo@gmail.com>
2021-01-02 00:00:02 +0900

    update

--- vue_hello2/resources/js/components/layout/TheHeader.vue
+++ vue_hello2/resources/js/components/layout/TheHeader.vue
@@ -3,7 +3,7 @@
     <div class="container text-center">
       <h1>4 Answers Quiz 4択クイズ&クイズ徹底解説</h1>
       <router-link to="/">
-        <img class="header-nav__logo" src="/public/images/header-logo.png" />
+        <img class="header-nav__logo" src="/images/header-logo.png" />
       </router-link>
     </div>
     <nav class="navbar navbar-default navbar-static-top">
@@ -76,6 +76,7 @@
     </nav>
   </header>
 </template>
+
 <script>
 export default {
   data() {
