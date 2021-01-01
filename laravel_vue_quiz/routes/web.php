eec8a53ef30c35f2c8cbcde4fa5927bc874edcb9
nousi <nousidayo@gmail.com>
2021-01-01 19:32:45 +0900

    web.phpにindexを読み込み

--- laravel_vue_quiz/routes/web.php
+++ laravel_vue_quiz/routes/web.php
@@ -17,3 +17,6 @@ Route::get('/', function () {
 Route::get('/quiz', function () {
     return view('quiz.index');
 });
+Route::get('/register', function () {
+    return view('auth.register.index');
+});
