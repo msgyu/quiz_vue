eb922efb42f7eae2ec335c3050a9fad2f94a529a
nousi <nousidayo@gmail.com>
2021-01-01 19:25:07 +0900

    import register

--- laravel_vue_quiz/resources/js/router.js
+++ laravel_vue_quiz/resources/js/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./components/page/Home";
 import Quiz from "./components/page/Quiz";
+import Register from "./components/page/Register";
 
 Vue.use(Router);
 
