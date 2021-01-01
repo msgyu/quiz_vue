101f0cebed909b1e342673dbe7c68c0a883ad201
nousi <nousidayo@gmail.com>
2021-01-01 18:58:26 +0900

    MainPage.vue をimport

--- laravel_vue_quiz/resources/js/app.js
+++ laravel_vue_quiz/resources/js/app.js
@@ -3,6 +3,8 @@ import router from "./router.js";
 import SocialSharing from "vue-social-sharing";
 import axios from "axios";
 import jQuery from "jquery";
+import MainPage from "./components/page/MainPage";
+
 window.$ = window.jQuery = jQuery;
 require("bootstrap");
 
