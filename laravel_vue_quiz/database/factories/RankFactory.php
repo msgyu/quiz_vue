fb4f1d8a7aaa675e8abf92d6ab5d10a8b36739da
nousi <nousidayo@gmail.com>
2021-01-16 19:14:21 +0900

    RankFactoryを更新

--- laravel_vue_quiz/database/factories/RankFactory.php
+++ laravel_vue_quiz/database/factories/RankFactory.php
@@ -7,6 +7,9 @@ use Faker\Generator as Faker;
 
 $factory->define(Rank::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'percentage_correct_answer' => rand(0, 10) * 10,
     ];
 });
