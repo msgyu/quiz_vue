e100ca244b88652d7adec85e14b20b47cbfceb76
nousi <nousidayo@gmail.com>
2021-01-16 17:26:37 +0900

    rankモデルを作成
    php artisan make:model Models/Rank -a を実行。
    そのため、コントローラーやFactoryなどが同時に生成されている。

---
+++ laravel_vue_quiz/database/factories/RankFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Rank;
+use Faker\Generator as Faker;
+
+$factory->define(Rank::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
