cbc492bb56904a96d1ed9c69727757ad37ebaddf
nousi <nousidayo@gmail.com>
2021-01-16 17:37:27 +0900

    UserとRankのリレーションを追加

--- laravel_vue_quiz/app/Models/Rank.php
+++ laravel_vue_quiz/app/Models/Rank.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Rank extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'percentage_correct_answer'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function insertScore(int $correctRatio)
+    {
+        $ranking = new Rank();
+        $ranking->percentage_correct_answer = $correctRatio;
+        $ranking->user_id = Auth::id();
+        $ranking->save();
+    }
 }
--- laravel_vue_quiz/app/User.php
+++ laravel_vue_quiz/app/User.php
@@ -10,6 +10,11 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function rank()
+    {
+        return $this->hasOne('App\Models\Rank');
+    }
+
     /**
      * The attributes that are mass assignable.
      *
