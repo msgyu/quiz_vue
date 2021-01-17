a470f5988ab7593dfdd7f1bbebbba457993b1cea
nousi <nousidayo@gmail.com>
2021-01-17 14:42:09 +0900

    ユーザー認証をrouter.jsに追加

--- laravel_vue_quiz/resources/js/router.js
+++ laravel_vue_quiz/resources/js/router.js
@@ -9,7 +9,7 @@ import Keyword from "./components/page/Keyword";
 
 Vue.use(Router);
 
-export default new Router({
+const router = new Router({
     mode: "history",
     routes: [
         {
@@ -35,7 +35,10 @@ export default new Router({
         {
             path: "/mypage",
             name: "mypage",
-            component: Mypage
+            component: Mypage,
+            meta: {
+                requiresAuth: true
+            }
         },
         {
             path: "/keyword",
@@ -44,3 +47,37 @@ export default new Router({
         }
     ]
 });
+
+router.beforeEach((to, from, next) => {
+    if (to.matched.some(rec => rec.meta.requiresAuth)) {
+        router.app.$http
+            .get("/api/user")
+            .then(response => {
+                const user = response.data;
+                if (user) {
+                    next();
+                } else {
+                    next({
+                        path: "/login"
+                    });
+                }
+            })
+            .catch(error => {
+                if (error.response.status === 401) {
+                    alert(
+                        "未認証のユーザーのためログイン画面でログインを行ってください"
+                    );
+                } else {
+                    alert(
+                        "予期しないエラーが発生しました。再度ログインを行ってください"
+                    );
+                }
+                next({
+                    path: "/login"
+                });
+            });
+    } else {
+        next();
+    }
+});
+export default router;
