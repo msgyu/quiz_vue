de0d897586f1e6ff93b96561852fdd0ae1c555f9
nousi <nousidayo@gmail.com>
2021-01-17 13:34:08 +0900

    app.jsに vue-loading-overlayをimport

--- laravel_vue_quiz/resources/js/app.js
+++ laravel_vue_quiz/resources/js/app.js
@@ -4,6 +4,8 @@ import SocialSharing from "vue-social-sharing";
 import axios from "axios";
 import jQuery from "jquery";
 import MainPage from "./components/page/MainPage";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
 
 window.$ = window.jQuery = jQuery;
 require("bootstrap");
